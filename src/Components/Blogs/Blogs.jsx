480253f2d82590466b84648e592ce96cf6445654
Mohammad-Hafez <mhafez296@gmail.com>
2024-01-04 00:13:59 +0200

    finish-blogs

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -2,44 +2,66 @@ import axios from 'axios'
 import React, { useEffect } from 'react'
 import { useQuery } from 'react-query'
 import {ApiBaseUrl , ImgBaseURL} from '../ApiBaseUrl'
-import Slider from 'react-slick';
-
+import { Carousel } from 'primereact/carousel';
+        
 export default function Blogs({setIsBlogsLoading}) {
-  let BestSettings = {
-    Infinity : false ,
-    lazyLoad: true,
-    speed: 500,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 3,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          initialSlide: 2
-        },
-      },
-    ],
-  };
 
   const getBlogs = ()=>{
-    return axios.get(ApiBaseUrl + `blogs`)
-  }
-  let {data , isLoading} = useQuery('Blogs-Slider' , getBlogs)
+    return axios.get(ApiBaseUrl + `blogs`);
+  };
+
+  let {data , isLoading} = useQuery('Blogs-Slider' , getBlogs);
+
+  let allBlogs = data?.data?.data?.data;
+
+  console.log(data?.data.data.data);
+
   useEffect(()=>{
     setIsBlogsLoading(isLoading)},
-    [isLoading , setIsBlogsLoading])
+    [isLoading , setIsBlogsLoading]);
+
+    const responsiveOptions = [
+      {
+          breakpoint: '1199px',
+          numVisible: 1,
+          numScroll: 1
+      },
+      {
+          breakpoint: '991px',
+          numVisible: 2,
+          numScroll: 1
+      },
+      {
+          breakpoint: '767px',
+          numVisible: 1,
+          numScroll: 1
+      }
+  ];
+  let blogTemplate = (blog)=>{
+    return(
+      <div key={blog._id} className='my-2 d-flex justify-content-center align-item-center p-3 normal-blue-text h-100'>
+       <div className="blogImage">
+         <img src={ImgBaseURL + blog.image} className='img-fluid rounded' alt={blog.title + 'image'} />
+       </div>
+       <div className="blogInfo grey-brdr my-4 rounded-end p-4 border-start-0 d-flex flex-column">
+         <span className="blogDate grey-brdr px-4 py-1 rounded-pill mb-3">
+           {blog.createdAt.slice(0, 10)}
+         </span>
+         <div className="blogTitle mb-4">
+          <h5>{blog.title}</h5>
+         </div>
+         <p>{blog.content}</p>
+         <p>{blog.author}</p>
+       </div>
+     </div>
+    )
+  }
   return <>
   <div className="blogs-slider-Container light-grey-bg mb-5">
     <div className="container">
-
+      {allBlogs &&
+        <Carousel value={allBlogs} numVisible={3} numScroll={3} responsiveOptions={responsiveOptions} itemTemplate={blogTemplate} />
+      }
     </div>
   </div>
     </>
--- src/Components/EmailTab/EmailTab.jsx
+++ src/Components/EmailTab/EmailTab.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+
 export default function EmailTab() {
   return <>
-  <div className="email-tab rounded-pill py-4">
+  <div className="email-tab rounded-pill py-3 mb-3 container-fluid">
     <div className="container d-flex align-items-center justify-content-between">
       <h6 className='m-0 text-white'>
       Subscribe to Our Prize  -  get a <span className=' text-decoration-underline'>200 JOD Coupon</span> for your first order! 
       </h6>
-      <div className="emailInputContainer">
-        
+      <div className="emailInputContainer position-relative">
+        <InputText
+          placeholder="Enter your email address"
+          // value={SearchVal}
+          className="position-relative border-0 ms-3 w-100"
+          onChange={(e) => {
+            // setSearchVal(e.target.value);
+          }}
+        />
+        <Button
+          icon="pi pi-send"
+          className="main-orange-bg border-0 fs-6 d-flex justify-content-center rounded-end-pill text-light position-absolute top-0 end-0 send-email"
+          // onClick={handleNavSearch}
+          // disabled={!SearchVal}
+        />
+
       </div>
     </div>
   </div>
