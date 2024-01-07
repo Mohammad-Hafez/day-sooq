5453a671143d55ffa3d05ea6191120efa7404190
Mohammad-Hafez <mhafez296@gmail.com>
2024-01-07 22:53:13 +0200

    all-products

--- src/Components/AllProducts/AllProducts.jsx
+++ src/Components/AllProducts/AllProducts.jsx
@@ -4,6 +4,8 @@ import SideMenu from '../SideMenu/SideMenu';
 import { useQuery } from 'react-query';
 import axios from 'axios';
 import { ApiBaseUrl } from '../ApiBaseUrl';
+import Loader from '../Loader/Loader'
+import ProductCard from '../ProductCard/ProductCard'
 
 export default function AllProducts() {
 const [FilterMethod, setFilterMethod] = useState();
@@ -21,17 +23,22 @@ let {data , isFetching , isLoading , refetch } = useQuery('get-products' , getAl
       <title>All Products</title>
     </Helmet>
     <div className="container">
+      {isLoading ? <Loader/> : <>
       <div className="row mt-3 mb-4 gy-3">
         <div className="col-3">
           <SideMenu setFilterMethod={setFilterMethod} FilterMethod={FilterMethod}/>
         </div>
         <div className="col-9">
-          <div className="row">
-            <div className="col-6 col-sm-4 col-md-3">
-            </div>
+          <div className="row gy-3">
+            {data?.data.data.data.map((product)=> <div key={product?._id} className="col-6 col-sm-4 col-md-3">
+                <ProductCard product={product} category={product?.isAction? 'bidding' : 'any'}/>
+              </div>
+            )}
           </div>
         </div>
       </div>
+      </> }
+      {isFetching && <Loader/>}
     </div>
     </>
 }
