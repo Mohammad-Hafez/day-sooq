200c7958573bc86af8f3fcd35c34f7e0fe11c9ad
Mohammad-Hafez <mhafez296@gmail.com>
2024-01-12 21:27:37 +0200

    finish-otp

--- src/Components/ForgetPassword/ForgetPassword.jsx
+++ src/Components/ForgetPassword/ForgetPassword.jsx
@@ -22,7 +22,7 @@ export default function ForgetPassword() {
       setIsLoading(false);
         localStorage.setItem('forgetToken' , data.token);
         navigate('/PasswordOtp')
-        toast.success(`Login Successfully. Enjoy Your Journey`, {
+        toast.success(`Email Send Successfully. Get Your OTP`, {
           className: 'first-z mt-5 bg-main-light ',
           duration: 2000,
         });
@@ -50,10 +50,11 @@ export default function ForgetPassword() {
       <Helmet>
         <title>Forget Password</title>
       </Helmet>
-      <div className="container login w-75">
-        <form onSubmit={formik.handleSubmit} className="text-center">
+      <div className="container login my-3 px-4">
+        <form onSubmit={formik.handleSubmit} className="">
+        <h5 className='dark-blue-text fw-bolder ms-2'>Enter Your Recovery Email</h5>
           <div className="row">
-            <div className="col-8 offset-2 my-3 p-4">
+            <div className="col-12 col-md-6 offset-md-3 col-lg-4 offset-lg-4">
               <div className="w-75 mx-auto">
                 <div className="col-12 text-start">
                   <label htmlFor="email" className="ms-2">
@@ -82,7 +83,7 @@ export default function ForgetPassword() {
                   )}
                 </div>
 
-                <div className="btns col-12 my-2">
+                <div className="btns col-12 my-2 text-center">
                   {isLoading ? (
                     <button type="button" className="btn btn-orange rounded-pill text-light w-75 mx-auto">
                       <i className=" fa fa-spin fa-spinner"></i>
@@ -91,7 +92,7 @@ export default function ForgetPassword() {
                     <button
                       type="submit"
                       disabled={!(formik.isValid && formik.dirty)}
-                      className="btn btn-orange rounded-pill text-light mx-auto w-75 d-flex align-items-center justify-content-center"
+                      className="btn btn-orange rounded-pill text-light mx-auto w-75 "
                     >
                       <span className="me-2">Send Email</span>
                     </button>
--- src/Components/PasswordOtp/PasswordOtp.jsx
+++ src/Components/PasswordOtp/PasswordOtp.jsx
@@ -10,23 +10,31 @@ import { alertCircle } from 'react-icons-kit/feather/alertCircle';
 import toast from 'react-hot-toast';
 
 export default function PasswordOtp() {
-
   let navigate = useNavigate();
 
   const [ErrMsg, setErrMsg] = useState();
   const [isLoading, setIsLoading] = useState(false);
 
   const sendOtp = async (val) => {
+    setIsLoading(true)
     try {
-      let {data} = await axios.post(ApiBaseUrl + `verifyOtp/${localStorage.getItem('forgetToken')}` , val);
-      console.log(data);
+      let { data } = await axios.post(ApiBaseUrl + `auth/verifyOtp/${localStorage.getItem('forgetToken')}`, val);
+      toast.success('OTP verified successfully', {
+        className: 'first-z mt-5 bg-main-light ',
+        duration: 2000,
+      });
+      setIsLoading(false)
+      navigate('/ResetPassword');
     } catch (error) {
-      console.error(error)
+      setIsLoading(false)
+      console.error(error);
+      toast.error('Error verifying OTP. Please try again.');
+      setErrMsg('Error verifying OTP. Please try again.');
     }
-  }
+  };
 
   let mySchema = Yup.object({
-    otp: Yup.string().required('OTP is required'),
+    otp: Yup.string().required('OTP is required').matches(/^\d{4}$/, 'OTP must be exactly 4 digits'),
   });
 
   let formik = useFormik({
@@ -37,59 +45,61 @@ export default function PasswordOtp() {
     onSubmit: (values) => sendOtp(values),
   });
 
-  return <>
-    <Helmet>
-      <title>OTP Password</title>
-    </Helmet>
-    <div className="container login my-3">
+  return (
+    <>
+      <Helmet>
+        <title>OTP Password</title>
+      </Helmet>
+      <div className="container login my-3">
         <form onSubmit={formik.handleSubmit} className="text-center">
           <div className="row">
-            <div className="col-5 offset-4 my-3">
-                <div className="col-12 text-start">
-                  <label htmlFor="otp" className="ms-2">
-                    Enter OTP 
-                  </label>
-                  <input
-                    type="text"
-                    placeholder="Enter 4 digits"
-                    className="AuthForm-inputs mb-2 form-control"
-                    name="otp"
-                    id="otp"
-                    value={formik.values.otp}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                  />
-                  {formik.errors.otp && formik.touched.otp && (
-                    <div className="alert alert-danger">{formik.errors.otp}</div>
-                  )}
-                </div>
-
-                <div className="col-12">
-                  {ErrMsg && (
-                    <div className="Err alert alert-danger w-75 mx-auto">
-                      <Icon size={20} icon={alertCircle} /> {ErrMsg}
-                    </div>
-                  )}
-                </div>
-
-                <div className="btns col-12 my-2">
-                  {isLoading ? (
-                    <button type="button" className="btn btn-orange rounded-pill text-light w-75 mx-auto">
-                      <i className=" fa fa-spin fa-spinner"></i>
-                    </button>
-                  ) : (
-                    <button
-                      type="submit"
-                      disabled={!(formik.isValid && formik.dirty)}
-                      className="btn btn-orange rounded-pill text-light mx-auto w-75 d-flex align-items-center justify-content-center"
-                    >
-                      <span className="me-2">Verify OTP</span>
-                    </button>
-                  )}
-                </div>
+            <div className="col-12 col-md-6 offset-md-3 col-lg-4 offset-lg-4 my-3">
+              <div className="col-12 text-start">
+                <label htmlFor="otp" className="ms-2">
+                  Enter OTP
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter OTP received in email"
+                  className="AuthForm-inputs mb-2 form-control"
+                  name="otp"
+                  id="otp"
+                  value={formik.values.otp}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                {formik.errors.otp && formik.touched.otp && (
+                  <div className="alert alert-danger">{formik.errors.otp}</div>
+                )}
               </div>
+
+              <div className="col-12">
+                {ErrMsg && (
+                  <div className="Err alert alert-danger w-75 mx-auto">
+                    <Icon size={20} icon={alertCircle} /> {ErrMsg}
+                  </div>
+                )}
+              </div>
+
+              <div className="btns col-12 my-2 text-center">
+                {isLoading ? (
+                  <button type="button" className="btn btn-orange rounded-pill text-light w-75 mx-auto">
+                    <i className=" fa fa-spin fa-spinner"></i>
+                  </button>
+                ) : (
+                  <button
+                    type="submit"
+                    disabled={!(formik.isValid && formik.dirty)}
+                    className="btn btn-orange rounded-pill text-light mx-auto w-75"
+                  >
+                    <span className="me-2">Verify OTP</span>
+                  </button>
+                )}
+              </div>
+            </div>
           </div>
         </form>
       </div>
     </>
+  );
 }
