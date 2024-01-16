829ab02ac3521160d7803635af2f17c96b96b89a
Mohammad-Hafez <mhafez296@gmail.com>
2024-01-16 20:38:06 +0200

    show-reviews

--- src/Components/ProductReviews/ProductReviews.jsx
+++ src/Components/ProductReviews/ProductReviews.jsx
@@ -3,15 +3,22 @@ import React from 'react';
 import { useQuery } from 'react-query';
 import { ApiBaseUrl } from '../ApiBaseUrl';
 import StarRating from '../StarRating/StarRating';
+import { IoIosStar, IoIosStarOutline } from 'react-icons/io';
+
+const generateFixedRatings = (ratingReviews) => {
+  const fixedRatings = Array.from({ length: 5 }, (_, index) => {
+    const foundRating = ratingReviews.find((review) => review.rating === index + 1);
+    return foundRating ? foundRating.nunmberOfRating : 0;
+  });
+  return fixedRatings;
+};
 
 export default function ProductReviews({ product }) {
-  // Define a function to fetch product ratings
   const getRating = async () => {
     const response = await axios.get(ApiBaseUrl + `reviews/rating/${product._id}`);
-    return response.data; // Assuming the response contains the product ratings
+    return response.data;
   };
 
-  // Use React Query to fetch data
   const { data, isLoading, isError } = useQuery('get-ratings', getRating);
 
   return (
@@ -24,24 +31,31 @@ export default function ProductReviews({ product }) {
           {data && (
             <div>
               <div className="avrg d-flex align-items-center">
-              <p className='mb-0 me-2'>Average Rating:</p>
-              <StarRating averageRating={data.data.ratingReviews.reduce((acc, review) => acc + review.avgRating, 0) / data.data.ratingReviews.length} />
+                <p className="mb-0 me-2">Average Rating:</p>
+                <StarRating averageRating={
+                    data.data.ratingReviews.reduce((acc, review) => acc + review.avgRating, 0) /
+                    data.data.ratingReviews.length
+                  } />
+                <span className='ms-2 fs-6'>({data.data.ratingReviews.length} Reviews)</span>
               </div>
-              <p className='mb-0'>Ratings Distribution:</p>
-              <ul className=''>
-                {data.data.ratingReviews.map((review, index) => (
-                  <li key={index} className='d-flex m-0 p-0'>
-                    <StarRating averageRating={review.avgRating} /> 
-                    <p className='ms-2'>({review.nunmberOfRating})</p>
+              <p className="mb-0">Ratings Distribution:</p>
+              <ul className="distribution-rate">
+                {generateFixedRatings(data.data.ratingReviews).map((numberOfRating, index) => (
+                  <li key={index} className="d-flex m-0 p-0 star my-1 align-items-center">
+                    {[...Array(index + 1)].map((_, i) => (
+                      <IoIosStar key={i} />
+                    ))}
+                    {[...Array(5 - index - 1)].map((_, i) => (
+                      <IoIosStarOutline key={i} />
+                    ))}
+                    <span className="ms-2 light-grey-text">({numberOfRating})</span>
                   </li>
                 ))}
               </ul>
             </div>
           )}
         </div>
-        <div className="col-sm-6">
-          {/* Additional content for the second column, if needed */}
-        </div>
+        <div className="col-sm-6">{/* Additional content for the second column, if needed */}</div>
       </div>
     </div>
   );
