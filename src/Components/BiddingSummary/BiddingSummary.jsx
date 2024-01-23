99ac3c0562777923dd71c3f0b25e436dfd4862f8
Mohammad-Hafez <mhafez296@gmail.com>
2024-01-23 03:30:05 +0200

    finish-buyNow-style

--- src/Components/BiddingSummary/BiddingSummary.jsx
+++ src/Components/BiddingSummary/BiddingSummary.jsx
@@ -142,7 +142,7 @@ let Biddingformik = useFormik({
       </Dialog>
       <Dialog visible={BuyNowVisible} onHide={() => setBuyNowVisible(false)} style={{ width: 'fit-content' }} breakpoints={{ '960px': '75vw', '641px': '100vw' }}>
       <div className="dialogContainer px-5 d-flex flex-column justify-content-around align-items-center h-100">
-        <h6 className='fw-bolder'>
+        <h6 className='fw-bolder main-orange-text'>
           Purchase Confirmation
         </h6>
         <p>
