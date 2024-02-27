285f1e666c0e92c9ebcdc73f184f5b09960b382d
Mohamed Hafez <mhafez296@gmail.com>
2024-02-27 12:46:21 +0200

    handle main slider

--- src/Components/MainSlider/MainSlider.jsx
+++ src/Components/MainSlider/MainSlider.jsx
@@ -1,11 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 import Slider from "react-slick";
 import { useQuery } from 'react-query';
 import axios from 'axios';
-import {ApiBaseUrl , ImgBaseURL} from '../ApiBaseUrl'
-import { Carousel } from 'primereact/carousel';
+import { ApiBaseUrl, ImgBaseURL } from '../ApiBaseUrl';
 
-export default function MainSlider({setIsBannerLoading}) {
+export default function MainSlider({ setIsBannerLoading }) {
   let settings = {
     dots: true,
     infinite: true,
@@ -17,64 +16,31 @@ export default function MainSlider({setIsBannerLoading}) {
     autoplay: true,
     autoplaySpeed: 2000,
   };
-  const getBanners = ()=>{
-    return axios.get(ApiBaseUrl + `banners`)
-  }
-  let {data , isLoading } = useQuery('mainBanner' , getBanners )
+
+  const getBanners = () => {
+    return axios.get(ApiBaseUrl + `banners`);
+  };
+
+  let { data, isLoading } = useQuery('mainBanner', getBanners);
+  console.log(data?.data?.data?.data);
+
   useEffect(() => {
     setIsBannerLoading(isLoading);
   }, [isLoading, setIsBannerLoading]);
 
-  return <>
-  <div className="container mainSlider position-relative mb-3">
-        <Slider {...settings} className='rounded'>
-          <div className='firstSlide'>
-            <div className="row">
-              <div className="col-6 text-center d-flex align-items-center justify-content-center flex-column">
-                <div className="labelContainer d-flex align-items-center justify-content-center flex-column">
-                {/* <p className='fs-1 fw-light'>
-                  {data?.data?.data?.data[0].description}
-                  <br />
-                  
-                </p> */}
-                </div>
-              </div>
-              <div className="col-6">
-                <img alt='img1' src={ImgBaseURL +  data?.data?.data?.data[0]?.image} loading='lazy' className='img-fluid'/>
-              </div>
-            </div>
-          </div>
-          <div className='thirdSlide'>
-            <div className="row">
-              <div className="col-6 text-center d-flex align-items-center justify-content-center flex-column">
-                <div className="labelContainer d-flex align-items-center justify-content-center flex-column">
-                <p className='fs-1 fw-light'>
-                {data?.data?.data?.data[1]?.description}
-                </p>
-                </div>
-              </div>
-              <div className="col-6">
-                <img alt='img2' src={ImgBaseURL +  data?.data?.data?.data[1]?.image} className='img-fluid'/>
-              </div>
-            </div>
-          </div>
-          <div className='secSlide'>
-            <div className="row">
-              <div className="col-6 text-center d-flex align-items-center justify-content-center flex-column">
-                <div className="labelContainer d-flex align-items-center justify-content-center flex-column">
-                <p className='fs-1 fw-light'>
-                {data?.data?.data?.data[2]?.description}
-                </p>
-                </div>
-              </div>
-              <div className="col-6">
-              <img alt='img3' src={ImgBaseURL +  data?.data?.data?.data[2]?.image} className='img-fluid py-3'/>
-              </div>
-            </div>
-          </div>
-        </Slider>    
-        
+  // Filter data to get only slides where type === 'slider'
+  const sliderData = data?.data?.data?.data.filter(item => item.type === 'slider');
 
-  </div>
-        </>
+  return (
+    <div className="container mainSlider position-relative mb-3">
+      <Slider {...settings} className='rounded'>
+        {/* Map over the filtered slider data and create slides */}
+        {sliderData && sliderData.map((item, index) => (
+          <div key={index}>
+            <img src={ImgBaseURL + item.image} alt={`Slide ${index}`} />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
 }
